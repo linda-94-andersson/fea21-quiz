f65eeb26db01667feb8c02fe21acf9127846e064
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-23 09:55:40 +0100

    Löste one

--- react/src/challenges/One.jsx
+++ react/src/challenges/One.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Gör så att "times" börjar på 0 och ökar med 1 varje gång du trycker på den.
 
 const One = () => {
+  const [times, setTimes] = useState(0);
 
+  const handleTimes = () => {
+    setTimes(times + 1);
+  };
 
   // Ta inte bort data-testid
   return (
     <div>
       <p data-testid="one-text">Has been clicked {times} times.</p>
-      <button data-testid="one-button">Click me</button>
+      <button data-testid="one-button" onClick={handleTimes}>
+        Click me
+      </button>
     </div>
   );
 };
